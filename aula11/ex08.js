2cffc4f193c75782ba7002cbc4c2c720a3364abe
Marllon Silva Araujo Coelho <marllon.coelho@etec.sp.gov.br>
2021-12-09 12:59:01 -0300

    commit Aula10

---
+++ aula11/ex08.js
@@ -0,0 +1,6 @@
+var velo = 60.5
+console.log(`Velocidade do carro ${velo} Km/h`)
+if(velo > 60){
+    console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+}
+    console.log('Use cinto de segurança!')
---
+++ aula11/ex09.js
@@ -0,0 +1,8 @@
+var pais = 'Brasil'
+console.log(`Morando no ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Brasileiro')
+}
+else{
+    console.log('Estrangeiro')
+}
